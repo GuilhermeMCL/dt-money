94bc78a85c43e0374ce681ab7f6829652d6e52a2
GuilhermeMCL <guilhermemcl1503@gmail.com>
2025-01-03 13:30:27 -0300

    corrigindo sintaxe

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,40 +1,40 @@
 import { Header } from "../../components/header";
 import { Summary } from "../../components/Summary";
-import { PriceHighLight, TrasactionsContainer, TrasactionsTable } from "./styles";
+import {
+  PriceHighLight,
+  TrasactionsContainer,
+  TrasactionsTable,
+} from "./styles";
 
-export function Trasactions(){
-    return(
-        <div>
-            <Header/>
-            <Summary/>
+export function Trasactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
 
-    <TrasactionsContainer>
-    <TrasactionsTable>
-                <tbody>
-                    <tr>
-                        <td width="50%">Desenvolvimento de Site</td>
-                        
-                        <td>
-                        <PriceHighLight variant="incone">
-                            R$ 12.000,00
-                        </PriceHighLight>
-                        </td>
-                        <td>Venda</td>
-                        <td>13/04/2024</td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Comida</td>
-                        <td>
-                        <PriceHighLight variant="outcone">
-                            - R$ 60,00
-                        </PriceHighLight>
-                        </td>
-                        <td>Alimentação</td>
-                        <td>13/04/2024</td>
-                    </tr>
-                </tbody>
-            </TrasactionsTable>
-    </TrasactionsContainer>        
-        </div>
-    )
+      <TrasactionsContainer>
+        <TrasactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+
+              <td>
+                <PriceHighLight variant="incone">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Comida</td>
+              <td>
+                <PriceHighLight variant="outcone">- R$ 60,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2024</td>
+            </tr>
+          </tbody>
+        </TrasactionsTable>
+      </TrasactionsContainer>
+    </div>
+  );
 }
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -1,21 +1,20 @@
 import styled from "styled-components";
 
 export const TrasactionsContainer = styled.main`
-    widht:100%;
-    max-widht:1128px;
-    margin:4 auto 0 ;
-    padding: 0 1.5rem:
-
-
-`;
+    width:100%;
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 0 1.5rem;  
+    
+    `;
 
 export const TrasactionsTable = styled.table`
-    widht: 100%
+    width: 100%
     border-collapse: separate;
     border-spacing: 0 0.5rem;
     
     td { 
-        padding:1.25rem 2rem;
+        padding: 1.25rem 2rem;
         background:${props => props.theme["gray-700"]};
 
         &:first-child{
